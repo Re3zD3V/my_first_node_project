a8a93d7962b3cbf9d64060156f6cab9849f84733
Re3z <ghiz.k.dev@gmail.com>
2021-08-28 04:07:53 +0200

    Section 3.Responses: Added json response to app's '/json' path, installed hjs (hogan js) and hogan-middleware packages, set app's views folder path using 'path' module, set app's 'view engine' to 'mustache', set app's engine to 'mustache' using hogan-middleware.__express, added mustahe homepage and added response to app's '/home' path to render mustache home page with no data (null)

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require( 'express' );
+const path = require( 'path' );
+const hoganMiddleware = require( 'hogan-middleware' );
 
 const app = express();
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'mustache' );
+app.engine( 'mustache', hoganMiddleware.__express );
 
 app.get( '/', ( req, res, next ) => {
-	res.send( 'Hello !!' );
+	res.send( 'Hello !!!' );
+});
+
+app.get( '/json', ( req, res, next ) => {
+	const data = {
+		greetings : 'HELLO !'
+	};
+	res.json( data );
+});
+
+app.get( '/home', ( req, res, next ) => {
+	res.render( 'home', null );
 });
 
 app.listen(3000); // 3000, 5000, 8000, 8080
